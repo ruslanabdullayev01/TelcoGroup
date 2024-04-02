d9a7244ce8f877d03aec29e46cdb20f29bb2f8b8
ruslanabdullayev01 <ruslanabdullayev01@gmail.com>
2024-04-02 18:33:27 +0400

    ck editor for about and news description

--- TelcoGroup/Models/News.cs
+++ TelcoGroup/Models/News.cs
@@ -6,8 +6,8 @@ namespace TelcoGroup.Models
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        //public string? ImagePath { get; set; }
-        //[NotMapped] public IFormFile? Photo { get; set; }
+        public string? ImagePath { get; set; }
+        [NotMapped] public IFormFile? Photo { get; set; }
         public int? LanguageId { get; set; }
         public Language? Language { get; set; }
         public int LanguageGroup { get; set; }
