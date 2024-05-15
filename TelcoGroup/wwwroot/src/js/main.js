da1e6eb2ef611dc2aeed9e729303bce0817ee2a6
ruslanabdullayev01 <ruslanabdullayev01@gmail.com>
2024-05-15 21:22:26 +0400

    final

--- TelcoGroup/wwwroot/src/js/main.js
+++ TelcoGroup/wwwroot/src/js/main.js
@@ -85,42 +85,47 @@
 //    }
 //});
 
-document.addEventListener('DOMContentLoaded', function () {
-    var navlinks = document.querySelectorAll('.navlink');
+document.addEventListener("DOMContentLoaded", function () {
+    var navlinks = document.querySelectorAll(".navlink");
 
     navlinks.forEach(function (navlink) {
-        var sectionId = navlink.querySelector('a').getAttribute('href');
+        var sectionId = navlink.querySelector("a").getAttribute("href");
         var section = document.querySelector(sectionId);
 
-        navlink.addEventListener('click', function (event) {
-            event.preventDefault(); 
+        navlink.addEventListener("click", function (event) {
+            event.preventDefault();
 
             section.scrollIntoViewIfNeeded();
         });
     });
 
-    window.addEventListener('scroll', function () {
-        var sections = ['#Video', '#AboutHome', '#Services', '#Complexity', '#Contact', '#News', '#Partners'];
+    window.addEventListener("scroll", function () {
+        var sections = [
+            "#Video",
+            "#AboutHome",
+            "#Services",
+            "#Complexity",
+            "#Contact",
+            "#News",
+            "#Partners",
+        ];
 
         sections.forEach(function (value, index) {
             var section = document.querySelector(value);
             if (isElementInViewport(section)) {
                 navlinks.forEach(function (navlink) {
-                    navlink.classList.remove('activeNavLink');
+                    navlink.classList.remove("activeNavLink");
                 });
-                navlinks[index].classList.add('activeNavLink');
+                navlinks[index].classList.add("activeNavLink");
             }
         });
     });
 
     function isElementInViewport(element) {
         var rect = element.getBoundingClientRect();
-        return (
-            rect.top <= (window.innerHeight / 2)
-        );
+        return rect.top <= window.innerHeight / 2;
     }
 });
-
 
 //Mobile menu
 const mobileMenuBtn = document.querySelector(".mobileMenuBtn");
@@ -177,3 +182,64 @@ mobileLanguages.forEach((language) => {
         mobileAdditionalLanguageDiv.style.display = "none";
     });
 });
+
+// --------------------Mobile Slider
+
+const slider = document.querySelector("#Services .serviceCards");
+let startX;
+let currentSlide = 0;
+const slides = document.querySelectorAll("#Services .serviceCards .card");
+const numSlides = slides.length;
+const slideWidth = slides[0].clientWidth;
+const containerWidth = slider.parentElement.clientWidth;
+let isScrolling = false;
+
+function handleTouchStart(e) {
+    e.preventDefault();
+    isScrolling = true;
+    startX = e.touches[0].clientX;
+}
+
+function handleTouchMove(e) {
+    e.preventDefault();
+    if (!isScrolling) return;
+    const x = e.touches[0].clientX;
+    const move = startX - x;
+    slider.style.transition = "none";
+
+    slider.style.transform = `translateX(${-(
+        currentSlide * slideWidth +
+        move
+    )}px)`;
+}
+
+function handleTouchEnd(e) {
+    e.preventDefault();
+    isScrolling = false;
+    const endX = e.changedTouches[0].clientX;
+    const move = startX - endX;
+    slider.style.transition = "transform 0.3s ease";
+
+    if (Math.abs(move) > 50) {
+        if (move > 0) {
+            currentSlide = (currentSlide + 1) % numSlides;
+        } else {
+            currentSlide = (currentSlide - 1 + numSlides) % numSlides;
+        }
+    }
+    slider.style.transform = `translateX(${-currentSlide * slideWidth}px)`;
+}
+
+function loop(e) {
+    if (!isScrolling) {
+        currentSlide = (currentSlide + 1) % numSlides;
+        slider.style.transition = "transform 0.3s ease";
+        slider.style.transform = `translateX(${0}px)`;
+    }
+}
+
+loop();
+
+slider.addEventListener("touchstart", handleTouchStart);
+slider.addEventListener("touchmove", handleTouchMove);
+slider.addEventListener("touchend", handleTouchEnd);
